6f1e5ecf17cdad2b94ef9534e41111710b1c49b0
mioutie <139978917+mioutie@users.noreply.github.com>
2023-09-05 16:04:57 +0200

    nouveau js

---
+++ script1.js
@@ -0,0 +1,15 @@
+var nombre_inc=10;
+var nombre_entre;
+nombre_entre= prompt("quel est le inconnu?");
+if(nombre_entre> nombre_inc){
+    alert("trop grand");
+}
+if(nombre_entre< nombre_inc){
+    alert("trop petit");
+}
+if(nombre_entre>=nombre_inc){
+    alert("c'et top");
+}
+
+
+
---
+++ script2.js
@@ -0,0 +1,8 @@
+var age_limite= 18;
+var age_entre; 
+age_entre=prompt("quel est votre age?");
+if (age_entre<= age_limite){
+    alert("Bienvenue")
+}else{
+    alert("non!")
+}
