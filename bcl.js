492038d8bb1be0e5923474e444326fe1be3ca592
mioutie <139978917+mioutie@users.noreply.github.com>
2023-09-06 09:20:22 +0200

    exercice3js

--- bcl.js
+++ bcl.js
@@ -1,5 +1,15 @@
+// var n;
+// n = prompt("veuillez entrer un nombre n=");
+// for( i=0; i<=10; i++){
+//     document.write(n*1 + "<br>")
+// }
 var n;
-n = prompt("veuillez entrer un nombre n=");
-for( i=0; i<=10; i++){
-    document.write(n*1 + "<br>")
+var resultat;
+var compteur= 0;
+resultat=0;
+n = prompt("veuillez entrer un nombre entier n=");
+while(resultat < 1000){
+    resultat = n*compteur;
+    compteur= compteur +1;
+    document.write(resultat + "<br>");
 }
---
+++ script3.js
@@ -0,0 +1,17 @@
+var data_ = ["lundi", "mardi", "mercredi","jeudi", "vendredi", "samedi", "dimanche"];
+data_[7]=15;
+data_[8]= "un autre jour";
+
+console.log(data_.length);
+// document.write(data_[0]);
+// document.write(data_[1]);
+// document.write(data_[2]);
+// document.write(data_[3]);
+// document.write(data_[4]);
+// possible de faire une autre maniere comme en bas le deux sa marche
+// document.writeln(data_);
+document.write(data_[4]);
+alert(data_);
+
+
+
